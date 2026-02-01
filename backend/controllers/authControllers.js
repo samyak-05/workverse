cfa8701c4817c5bcbaa63425232601414f19a1ed
Samyak <samyaknigam.11@gmail.com>
2026-02-01 09:17:52 +0530

    hosting (samesite : strict -> none

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -33,7 +33,7 @@ export const signUp = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -70,7 +70,7 @@ export const signIn = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
